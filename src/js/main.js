6b942a7d8ada3ae4fcca0d2a3b98ab7555bfb987
João Vitor Menezes de Jesus <joao_vitor_mj@hotmail.com>
2022-08-28 01:42:42 -0300

    feat/some testes

--- src/js/main.js
+++ src/js/main.js
@@ -1,95 +1,141 @@
-const statusEl = document.getElementById('status');
-const dataEl = document.getElementById('data');
-const headersEl = document.getElementById('headers');
-const configEl = document.getElementById('config');
+const statusEl = document.getElementById("status");
+const dataEl = document.getElementById("data");
+const headersEl = document.getElementById("headers");
+const configEl = document.getElementById("config");
 
 const get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/posts', {
-        params: {
-            _limit: 5
-        }
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts", {
+      params: {
+        _limit: 5,
+      },
     })
-        .then((response) => {
-            renderOutput(response)
-        })
-}
+    .then((response) => {
+      renderOutput(response);
+    });
+};
 
 const post = () => {
-    console.log('post');
-}
+  const data = {
+    title: "teste",
+    body: "bar",
+    userId: 1,
+  };
+  axios
+    .post("https://jsonplaceholder.typicode.com/posts", data)
+    .then((response) => {
+      renderOutput(response);
+    });
+
+  console.log("post");
+};
 
 const put = () => {
-    console.log('put');
-}
+  const data = {
+    title: "teste1",
+    // body: "bar1",
+    userId: 1,
+  };
+  axios
+    .put("https://jsonplaceholder.typicode.com/posts/1", data)
+    .then((response) => {
+      renderOutput(response);
+    });
+
+  console.log("put");
+};
 
 const patch = () => {
-    console.log('patch');
-}
+  const data = {
+    title: "teste12",
+    body: "bar12",
+    userId: 1,
+  };
+  axios
+    .patch("https://jsonplaceholder.typicode.com/posts/1", data)
+    .then((response) => {
+      renderOutput(response);
+    });
+
+  console.log("patch");
+};
 
 const del = () => {
-    console.log('delete');
-}
+  axios.put("https://jsonplaceholder.typicode.com/posts/2").then((response) => {
+    renderOutput(response);
+  });
+  console.log("delete");
+};
 
 const multiple = () => {
-    console.log('multiple');
-}
+  Promise.all([
+    axios.get("https://jsonplaceholder.typicode.com/posts"),
+    axios.get("https://jsonplaceholder.typicode.com/users"),
+  ]).then((response) => {
+    console.log(response[0]);
+    console.log(response[1]);
+  });
+
+  console.log("multiple");
+};
 
 const transform = () => {
-    console.log('transform');
-}
+  console.log("transform");
+};
 
 const errorHandling = () => {
-    console.log('errorHandling');
-}
+  console.log("errorHandling");
+};
 
 const cancel = () => {
-    console.log('cancel');
-}
+  console.log("cancel");
+};
 
 const clear = () => {
-    statusEl.innerHTML = '';
-    statusEl.className = '';
-    dataEl.innerHTML = '';
-    headersEl.innerHTML = '';
-    configEl.innerHTML = '';
-}
+  statusEl.innerHTML = "";
+  statusEl.className = "";
+  dataEl.innerHTML = "";
+  headersEl.innerHTML = "";
+  configEl.innerHTML = "";
+};
 
 const renderOutput = (response) => {
-    // Status
-    const status = response.status;
-    statusEl.removeAttribute('class');
-    let statusElClass = 'inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium';
-    if (status >= 500) {
-        statusElClass += ' bg-red-100 text-red-800';
-    } else if (status >= 400) {
-        statusElClass += ' bg-yellow-100 text-yellow-800';
-    } else if (status >= 200) {
-        statusElClass += ' bg-green-100 text-green-800';
-    }
+  // Status
+  const status = response.status;
+  statusEl.removeAttribute("class");
+  let statusElClass =
+    "inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium";
+  if (status >= 500) {
+    statusElClass += " bg-red-100 text-red-800";
+  } else if (status >= 400) {
+    statusElClass += " bg-yellow-100 text-yellow-800";
+  } else if (status >= 200) {
+    statusElClass += " bg-green-100 text-green-800";
+  }
 
-    statusEl.innerHTML = status;
-    statusEl.className = statusElClass;
+  statusEl.innerHTML = status;
+  statusEl.className = statusElClass;
 
-    // Data
-    dataEl.innerHTML = JSON.stringify(response.data, null, 2);
-    Prism.highlightElement(dataEl);
+  // Data
+  dataEl.innerHTML = JSON.stringify(response.data, null, 2);
+  Prism.highlightElement(dataEl);
 
-    // Headers
-    headersEl.innerHTML = JSON.stringify(response.headers, null, 2);
-    Prism.highlightElement(headersEl);
+  // Headers
+  headersEl.innerHTML = JSON.stringify(response.headers, null, 2);
+  Prism.highlightElement(headersEl);
 
-    // Config
-    configEl.innerHTML = JSON.stringify(response.config, null, 2);
-    Prism.highlightElement(configEl);
-}
+  // Config
+  configEl.innerHTML = JSON.stringify(response.config, null, 2);
+  Prism.highlightElement(configEl);
+};
 
-document.getElementById('get').addEventListener('click', get);
-document.getElementById('post').addEventListener('click', post);
-document.getElementById('put').addEventListener('click', put);
-document.getElementById('patch').addEventListener('click', patch);
-document.getElementById('delete').addEventListener('click', del);
-document.getElementById('multiple').addEventListener('click', multiple);
-document.getElementById('transform').addEventListener('click', transform);
-document.getElementById('cancel').addEventListener('click', cancel);
-document.getElementById('error').addEventListener('click', errorHandling);
-document.getElementById('clear').addEventListener('click', clear);
+document.getElementById("get").addEventListener("click", get);
+document.getElementById("post").addEventListener("click", post);
+document.getElementById("put").addEventListener("click", put);
+document.getElementById("patch").addEventListener("click", patch);
+document.getElementById("delete").addEventListener("click", del);
+document.getElementById("multiple").addEventListener("click", multiple);
+document.getElementById("transform").addEventListener("click", transform);
+document.getElementById("cancel").addEventListener("click", cancel);
+document.getElementById("error").addEventListener("click", errorHandling);
+document.getElementById("clear").addEventListener("click", clear);
